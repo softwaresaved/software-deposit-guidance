e9864691e4483008e5051e4d2c61dc5ac8d54c2f
mikej888 <michaelj@epcc.ed.ac.uk>
2018-07-23 05:20:41 -0700

    Added scripts/word_to_markdown.sh

---
+++ scripts/word_to_markdown.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Usage: source work_to_markdown.sh *.docx
+#
+# Uses w2m to convert <FILE>.docx into markdown/<FILE>.md.
+# If markdown/ does not exist, it is created.
+# If <FILE>.md exists, it is overwritten.
+
+FILES=$@
+echo $FILES
+
+mkdir -p markdown
+for file in $@; do
+    echo "Converting $file"
+    filename=$(basename -- "$file")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    mdfilename="markdown/"$filename".md"
+    w2m $i > $mdfilename
+done
