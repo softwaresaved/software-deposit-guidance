759178649700f2db10361762f015c293581b0f98
mikej888 <michaelj@epcc.ed.ac.uk>
2018-07-24 09:25:53 -0700

    Updated comments

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,21 +1,41 @@
 #!/usr/bin/env python
 
 """
-Filter to replace tokens with values. This filter expects to
-find a PANDOC_CONFIG environment variable with the location a YAML
-file which contains tokens and the values to replace these.
+Program to replace tokens with values. Usage:
 
-If a token of form %TOKEN% is found in a document being processed by
-Pandoc, then TOKEN is sought in the tokens. If found then the token is
-replace by its value.
+    python filter.py <CONFIG_FILE> <INPUT_FILE> <OUTPUT_FILE>
 
-A special token "%DATE%" is replaced with today's date in form, for
-example, "23 July 2018".
+where:
+
+* <CONFIG_FILE>: a YAML file with keys and values only. Values
+  must  be strings, integers, floats or booleans only.
+* <INPUT_FILE>: a plain-text file.
+* <OUTPUT_FILE>: <INPUT_FILE> with all occurrences of tokens
+  replaced by values.
+
+For every key, TOKEN, in <CONFIG_FILE>, if %TOKEN% is found in
+<INPUT_FILE> it is replaced with the value corresponding to
+the key.
+
+Any occurrences of a special token "%DATE%" are replaced with
+the current date in form, for example, "23 July 2018".
 """
 
+# Copyright (c) 2018 The University of Edinburgh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import datetime
-import os
 import re
 import sys
 import yaml
@@ -24,26 +44,54 @@ import yaml
 PATTERN = re.compile("%.*?%")
 
 
-def load_config(config_file):
-    with open(config_file, "r") as f:
-        user_config = yaml.load(f)
-    config = {}
-    for (key, value) in user_config.iteritems():
-        config["%%%s%%" % key] = value
-    config["%DATE%"] = datetime.datetime.now().strftime("%d %B %Y")
-    return config
+def load_tokens(token_file):
+    """
+    Load keys and values from YAML file, token_file. Values must be
+    strings, integers, floats or booleans only.
+    For every TOKEN:VALUE in in file, a key-value pair,
+    %TOKEN%,VALUE, is added to a dict that is returned. A special
+    key-value pair, %DATE%,<CURRENT-DATE> is also added, where
+    the current date is of form, for example, "23 July 2018".
+
+    :param token_file: file name
+    :type token_file: str or unicode
+    :return: tokens and values
+    :rtype: dict
+    """
+    with open(token_file, "r") as f:
+        user_tokens = yaml.load(f)
+    tokens = {}
+    for (key, value) in user_tokens.iteritems():
+        tokens["%%%s%%" % key] = value
+    tokens["%DATE%"] = datetime.datetime.now().strftime("%d %B %Y")
+    return tokens
 
 
-def preconfig(config, src, dst):
+def replace_tokens(tokens, src, dst):
+    """
+    Read an input file, replace tokens with values, write the
+    updated file.
+
+    Read src and for every occurrence of a string of form %...%,
+    look for a corresponding token in tokens. If present, replace
+    the string with the corresponding value.
+
+    :param tokens: tokens and values
+    :rtype tokens: dict
+    :param src: input file name
+    :type src: str or unicode
+    :param src: output file name
+    :type src: str or unicode
+    """
     filtered = []
     with open(src, "r") as f:
         lines = f.readlines()
     for line in lines:
         matches = re.findall(PATTERN, line)
         for match in matches:
-            if match in config:
+            if match in tokens:
                 # Convert to str in case value is a number
-                line = line.replace(match, str(config[match]))
+                line = line.replace(match, str(tokens[match]))
         filtered.append(line)
     with open(dst, "w") as f:
         for line in filtered:
@@ -51,5 +99,5 @@ def preconfig(config, src, dst):
 
 
 if __name__ == "__main__":
-    config = load_config(sys.argv[1])
-    preconfig(config, sys.argv[2], sys.argv[3])
+    tokens = load_tokens(sys.argv[1])
+    replace_tokens(tokens, sys.argv[2], sys.argv[3])
